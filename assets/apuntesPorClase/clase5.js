419212bc1c6c25ab44ecb5b0080aea1b8b8e7b66
Felipe Muñoz <analista.felipe.m@gmail.com>
2023-03-17 13:28:19 -0300

    Clase 5 terminada.

---
+++ assets/apuntesPorClase/clase5.js
@@ -0,0 +1,22 @@
+/**
+ * Para el desarrollo del juego, se debera trabajar con arreglos dentro de arreglos.
+ */
+
+/**
+ * arreglos multidemencionales
+ * son arreglos dentro de arreglos dentro de arreglos etc.
+ * esto se utiliza en juegos 3D
+ */
+
+/**
+ * arreglos bidimencionales
+ * son arreglos dentro de arreglos (Son 2 por eso eso es bi)
+ * esto es lo que se usa en juegos 2D.
+ */
+
+/**
+ * en el archivo mapas.js existen arreglos bidimencionales, que son los mapas del juego, estan hechos en cuadrillas de 10x10
+ * para entenderlo mejor:
+ * cada mapa sera un arreglo de filas y este a su vez sera un arreglo de columnas.
+ * Map[Row["Col"]]
+ */
